2f047137aaae10a8f2f3cf3b0718416f8ccac8b9
Victor Lamers <vgl@sieg-ad.com.br>
2022-03-11 12:10:51 -0300

    Add Products

--- index2.js
+++ index2.js
@@ -3,9 +3,35 @@ let correios = new Correios();
 
 let products = [
   {
-    name: 'Reale Napa Bend',
-    
-  }
+    name: 'Reale Napa Red Blend',
+    price: '349,90',
+    height: '36',
+    length: '29',
+    width: '20',
+    diameter: '20',
+    weight: '1.5',
+    quantity: '1'
+  },
+  {
+    name: 'Disaronno',
+    price: '233,99',
+    height: '36',
+    length: '26',
+    width: '20',
+    diameter: '20',
+    weight: '0.7',
+    quantity: '3'
+  },
+  {
+    name: 'Marco Real',
+    price: '49,99',
+    height: '8',
+    length: '33',
+    width: '8',
+    diameter: '8',
+    weight: '1.5',
+    quantity: '1'
+  },
 ]
 
 let argsSedex = {
